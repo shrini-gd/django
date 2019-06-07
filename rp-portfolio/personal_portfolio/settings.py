e38c1ae7171b433ab3fa8bfcf9db92eb11a9cbe0
Shrinivas <shrini_deshpande@yahoo.co.uk>
2019-06-07 16:33:13 +0530

    details added version

--- rp-portfolio/personal_portfolio/settings.py
+++ rp-portfolio/personal_portfolio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    "/Users/Shrinivas/learn-django/rp-portfolio/projects/static/img/"
+]
